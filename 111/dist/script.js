3642130fda0679c5a1a532142c66c749d48dc8f7
pengmax <131646586+pengmax@users.noreply.github.com>
2023-05-29 14:55:56 +0800

    Update script.js

--- 111/dist/script.js
+++ 111/dist/script.js
@@ -1,1 +1,8 @@
 document.write("HELLO我是彭郁宸")
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count+=2;
+      counterElement.textContent = count;
+    }
